2f01d9bff685611a6297f02a0165b66b313c64d2
David Mayes <davidrmayes@gmail.com>
2022-01-22 21:08:51 -0700

    Updated HR app

---
+++ src/app/components/analytics-table/analytics-table.component.ts
@@ -0,0 +1,34 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Employee } from 'src/app/interfaces/employee';
+import { EmployeeService } from 'src/app/services/employee.service';
+
+@Component({
+  selector: 'app-analytics-table',
+  templateUrl: './analytics-table.component.html',
+  styleUrls: ['./analytics-table.component.scss']
+})
+export class AnalyticsTableComponent implements OnInit {
+  @Input()
+  departmentId: string;
+
+  weekdays: string[] = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday'];
+  employees: Employee[] = [];
+  employeeData: Employee[] = [];
+
+  constructor(
+    private employeeService: EmployeeService
+  ) { }
+
+  ngOnInit(): void {
+    // this.employees = this.employeeData.filter(employee => employee.departmentId === this.departmentId);
+    this.employeeService.getEmployeeHoursByDepartment(this.departmentId).subscribe((employees: Employee[]) => {
+      this.employees = employees;
+    });
+  }
+
+  getTotalHours(employee: Employee): number {
+    return employee.monday + employee.tuesday + employee.wednesday
+        + employee.thursday + employee.friday + employee.saturday + employee.sunday;
+  }
+
+}
